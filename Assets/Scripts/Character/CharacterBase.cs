23594b03bfb711dbb03d254def6abd18d1ece0c0
Choi <456tjdrb@gmail.com>
2026-04-28 20:49:40 +0900

    feat: 기존 idle, shoot에 reload 이미지를 추가함으로 isForceReloading에서 enum으로 관리방법 바꿈

--- Assets/Scripts/Character/CharacterBase.cs
+++ Assets/Scripts/Character/CharacterBase.cs
@@ -13,21 +13,24 @@ public abstract class CharacterBase : MonoBehaviour
     [SerializeField] protected float maxShield;
     [SerializeField] protected bool buff;
     [SerializeField] protected bool debuff;
-    [SerializeField] private bool crunch;
+    
     [SerializeField] protected float skillCoolTime;
     [SerializeField] protected float burstCoolTime;
     [SerializeField] protected float reloadTime;
     [SerializeField] private bool survive;
     [SerializeField] protected float chargingBurstGauge;
     [SerializeField] protected float attackDamage;
+    [SerializeField] private Sprite idleSprite;   // 대기 이미지
+    [SerializeField] private Sprite shootSprite;  // 사격 이미지
+    [SerializeField] private Sprite reloadSprite; // 리로딩 이미지
+    private CharacterState currentState { get; set; }
+    private SpriteRenderer spriteRenderer;
 
     public bool IsAlive => survive;
     public float HpRatio => hp / maxHp;
     public float MaxBulletCount => maxBulletCount;
     public int CurrentBulletCount => bulletCount;
     public float ShieldRatio => shield / maxShield;
-
-    public bool isForceReloading { get; private set; }
 
     public abstract void Initialize();
     public abstract void UseSkill();
@@ -41,32 +44,39 @@ public abstract class CharacterBase : MonoBehaviour
             {
                 damage *= 0.75f;
             }
-            //디퍼브 고려
+            //디버프 고려
             if (debuff)
             {
                 damage *= 1.25f;
             }
 
-            if (crunch) //엄폐 중이면 쉴드를 우선 깎음
+            // 1. idle일 때는 쉴드가 먼저 깎이고, 쉴드가 깨지면 남은 데미지가 hp에 적용
+            // 2. fire일 때는 바로 hp에 데미지 적용
+            // 3. 리로딩 중일 때는 fire와 동일하게 hp에 데미지 적용 (리로딩이 엄폐 상태라고 가정)
+            switch(currentState)
             {
-                if(shield > 0) //쉴드가 남아 있음
-                {
-                    shield -= damage;
-                    //체력 감소
-                    if(shield < 0) //쉴드 깨짐 남은 데미지 받음
+                case CharacterState.Idle:
+                    if(shield > 0) //쉴드가 남아 있음
                     {
-                        hp += shield;
-                        shield = 0;
+                        shield -= damage;
+                        //체력 감소
+                        if(shield < 0) //쉴드 깨짐 남은 데미지 받음
+                        {
+                            hp += shield;
+                            shield = 0;
+                        }
                     }
-                }
-                else //쉴드 깨짐
-                {
+                    else //쉴드 깨짐
+                    {
+                        hp -= damage;
+                    }
+                    break;
+                case CharacterState.Fire:
                     hp -= damage;
-                }
-            }
-            else
-            {
-                hp -= damage;
+                    break;
+                case CharacterState.Reload:
+                    hp -= damage;
+                    break;
             }
             
             //사망 여부   
@@ -82,14 +92,15 @@ public abstract class CharacterBase : MonoBehaviour
         // 사격 조건 체크
         if (survive)
         {
-            if (!isForceReloading && bulletCount > 0) //강제 리로딩 중이 아니고 탄창이 남아 있는 경우에만 사격
+            if (currentState != CharacterState.Reload && bulletCount > 0) //강제 리로딩 중이 아니고 탄창이 남아 있는 경우에만 사격
             {
+                spriteRenderer.sprite = shootSprite;
+                currentState = CharacterState.Fire;
                 bulletCount--;
                 // 사격 로직 (예: 총알 발사, 애니메이션 재생 등)
                 Debug.Log("사격");
                 if(bulletCount == 0) //탄창이 다 떨어졌으면 강제 리로딩 상태로 전환
                 {
-                    isForceReloading = true;
                     Debug.Log("탄창이 다 떨어졌습니다. 강제 리로딩 상태로 전환합니다.");
                     TryReload();
                 }
@@ -106,24 +117,42 @@ public abstract class CharacterBase : MonoBehaviour
     {
         // 호출 전 survive 체크가 보장되므로 중복 체크 생략
         // 리로딩 조건 체크
-        if(bulletCount == maxBulletCount) return;
+        if(bulletCount == maxBulletCount)
+        {
+            currentState = CharacterState.Idle;
+            spriteRenderer.sprite = idleSprite;
+            return;
+        }
+        if(currentState == CharacterState.Reload) return;
         StartCoroutine(ReloadDelay());
         // 이후 리로딩 애니메이션 재생 추가할 예정
     }
 
     private IEnumerator ReloadDelay()
     {
+        currentState = CharacterState.Reload;
+        spriteRenderer.sprite = reloadSprite;
         // 리로딩 시간 대기
         yield return new WaitForSeconds(reloadTime);
         bulletCount = maxBulletCount;
-        isForceReloading = false;
+        currentState = CharacterState.Idle;
+        spriteRenderer.sprite = idleSprite;
         Debug.Log("리로딩 완료. 사격 가능");
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        //SpriteRenderer 같은 컴포넌트 참조는 Awake에서 처리
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     void Start()
     {
         Initialize();
+    }
+
+    void OnEnable()
+    {
+        
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Core/CharacterState.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+// 캐릭터의 상태를 나타내는 열거형
+// 캐릭터 상태를 새 파일에 넣는 이유는 어디서든 참조할 수 있게 하기 위해서
+// 적 AI에서 캐릭터가 사격상태일 때 공격하는 것이 shield가 아닌 hp를 깎을 수 있어서 효과적
+public enum CharacterState
+{
+    Idle,
+    Fire,
+    Reload
+}
